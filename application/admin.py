54f3924878e517e62b92ff079b1d4ff781b0ff76
Kowit C <kowit@cookly.me>
2016-03-16 09:05:57 +0700

    fix link

--- application/admin.py
+++ application/admin.py
@@ -3,7 +3,9 @@ from application.models import Application
 
 
 class ApplicationAdmin(admin.ModelAdmin):
-    list_display = ('name', 'position', 'university',
+    list_display = ('name', 'email', 'position', 'university',
                     'start_date', 'end_date', 'semester_year', 'gpa',)
+    search_fields = ['email', ]
+
 
 admin.site.register(Application, ApplicationAdmin)
