5e2dbca3ee032a9eab84ca685fb3d9b456e659bd
Kowit C <kowit@cookly.me>
2016-03-16 22:59:36 +0700

    change filter

--- theinternship/settings.py
+++ theinternship/settings.py
@@ -1,5 +1,8 @@
 import os
-import urlparse
+try:
+    import urlparse
+except:
+    import urllib.parse as urlparse
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
